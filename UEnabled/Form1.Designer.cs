21bde8c51f607ca8e38c15c31cbe26fe193828c3
Kevin Reiter <reiter.kk@gmail.com>
2020-09-11 17:51:47 +0200

    Add project files.

---
+++ UEnabled/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace UEnabled
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdLoeschen = new System.Windows.Forms.Button();
+            this.LstLaender = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // CmdLoeschen
+            // 
+            this.CmdLoeschen.Location = new System.Drawing.Point(188, 29);
+            this.CmdLoeschen.Name = "CmdLoeschen";
+            this.CmdLoeschen.Size = new System.Drawing.Size(75, 23);
+            this.CmdLoeschen.TabIndex = 0;
+            this.CmdLoeschen.Text = "Loeschen";
+            this.CmdLoeschen.UseVisualStyleBackColor = true;
+            this.CmdLoeschen.Click += new System.EventHandler(this.CmdLoeschen_Click);
+            // 
+            // LstLaender
+            // 
+            this.LstLaender.FormattingEnabled = true;
+            this.LstLaender.ItemHeight = 15;
+            this.LstLaender.Location = new System.Drawing.Point(12, 29);
+            this.LstLaender.Name = "LstLaender";
+            this.LstLaender.Size = new System.Drawing.Size(153, 184);
+            this.LstLaender.TabIndex = 1;
+            this.LstLaender.SelectedIndexChanged += new System.EventHandler(this.LstLaender_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LstLaender);
+            this.Controls.Add(this.CmdLoeschen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdLoeschen;
+        private System.Windows.Forms.ListBox LstLaender;
+    }
+}
+
---
+++ UEnabled/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UEnabled
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LstLaender.Items.Add("Lichtenstein");
+            LstLaender.Items.Add("Malta");
+            LstLaender.Items.Add("Andorra");
+            LstLaender.Items.Add("San Marino");
+            LstLaender.Items.Add("Monacco");
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            CmdLoeschen.Enabled = false;
+        }
+
+        private void LstLaender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (LstLaender.SelectedIndices.Count == 0 || LstLaender.Items.Count == 0)
+            {
+                CmdLoeschen.Enabled = false;
+            }
+            else
+            {
+                CmdLoeschen.Enabled = true;
+            }
+        }
+
+        private void CmdLoeschen_Click(object sender, EventArgs e)
+        {
+            int index = LstLaender.SelectedIndex;
+            LstLaender.Items.RemoveAt(index);
+        }
+    }
+}
